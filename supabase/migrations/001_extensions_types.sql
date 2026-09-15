d845c5a39a3e368e6e2bf700f27acfa30d5bf7e8
Jose-Huyema <josecsyda@gmail.com>
2026-09-15 00:23:10 -0300

    Delete supabase directory

--- supabase/migrations/001_extensions_types.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TYPE user_role AS ENUM ('admin', 'profe');
-CREATE TYPE attendance_status AS ENUM ('presente', 'ausente', 'justificado');
-CREATE TYPE movement_type AS ENUM ('ingreso', 'egreso', 'baja_desgaste');
